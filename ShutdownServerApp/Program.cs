57a31d3fa699ff7f4cea2c3f038d83af9c31facb
Mehdi Oulad Khlie <mehdi.ouladkhlie@icloud.com>
2025-04-04 08:17:05 +0200

    Add RoundedPanel class for custom UI component and remove obsolete comments in Program.cs

--- ShutdownServerApp/Program.cs
+++ ShutdownServerApp/Program.cs
@@ -8,9 +8,6 @@ namespace ShutdownServerApp
         [STAThread]
         static void Main()
         {
-            // Als ApplicationConfiguration.Initialize() fouten geeft, vervang dan met:
-            // Application.EnableVisualStyles();
-            // Application.SetCompatibleTextRenderingDefault(false);
             ApplicationConfiguration.Initialize();
             Application.Run(new MainForm());
         }
